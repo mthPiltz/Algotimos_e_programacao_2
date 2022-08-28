eaf2471d6e0a4ab52900518217fa984968fbca24
Piltz <mpiltz14@gmail.com>
2022-08-27 22:06:18 -0400

    ex3 pronto

--- lista_avaliativa/ex3.cpp
+++ lista_avaliativa/ex3.cpp
@@ -7,12 +7,14 @@ struct eletrodomestico{
 };
 
 double calculaConsumototal(eletrodomestico e[5], double tempo);
-double calculaPorcentagem(double total, eletrodomestico e[5]);
+double porcentagem(double valorTotal, double valorParcial);
 
 int main(){
     eletrodomestico eletrodomesticos[5];
     int tempoLigado;
     double consumo;
+    double valorParcial;
+    double porcentagemConsumo;
 
     for(int i = 0; i < 5;i++){
         scanf("%s", eletrodomesticos[i].nome);
@@ -21,11 +23,22 @@ int main(){
     scanf("%d", &tempoLigado);
 
     consumo = calculaConsumototal(eletrodomesticos, tempoLigado);
-    printf("%.2lf", consumo * tempoLigado);
+    printf("%.2lf\n", consumo);
+
+    for(int i = 0; i < 5; i++){
+        valorParcial = eletrodomesticos[i].potencia * eletrodomesticos[i].tempo * tempoLigado;
+        porcentagemConsumo = porcentagem(consumo, valorParcial);
+        printf("%.2lf\n", porcentagemConsumo);
+    }
 }
 
-double calculaPorcentagem(double total, eletrodomestico e[5]){
-    
+double porcentagem(double valorTotal, double valorParcial){
+    double auxiliarMultiplicacaoCruzada;
+    double porcentagemValorParcial;
+    int cem = 100;
+    auxiliarMultiplicacaoCruzada = valorParcial * cem;
+    porcentagemValorParcial = auxiliarMultiplicacaoCruzada / valorTotal;
+    return porcentagemValorParcial;
 }
 
 double calculaConsumototal(eletrodomestico e[5], double tempo){
@@ -34,5 +47,5 @@ double calculaConsumototal(eletrodomestico e[5], double tempo){
         resultado = resultado + e[i].potencia * e[i].tempo;
     }
     // printf("%lf", &resultado);
-    return resultado;
+    return resultado * tempo;
 }
