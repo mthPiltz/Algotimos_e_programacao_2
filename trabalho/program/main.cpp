7f461f979a932cb678bfa80590b2a42cd3febc8d
Piltz <matheus.piltz@ufms.br>
2022-10-18 21:50:30 -0400

    estudo da leitura de arquivos

--- trabalho/program/main.cpp
+++ trabalho/program/main.cpp
@@ -1,17 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+struct acertos {
+    acertos* next;
+    char notas[30];
+};
+
+void adiciona(acertos* acerto, char teste[30]);
+
 int main(){
     FILE * file;
-    file = fopen("numero.txt", "r");
-    int x, y, z;
+    file = fopen("acertos.txt", "r");
+    char teste[30];
 
     if(file == NULL){
         system("pause");
         return 0;
     }
 
-    fscanf(file, "%i %i %i", &x, &y, &z);
-    printf("%i %i %i", x, y, z);
+    int i = 0;
+    acertos* acerto;
+
+    while (fgets(teste, 1500, file) != NULL)
+    {
+        adiciona(acerto, teste);
+        break;
+    }
+    
+    printf("[%s]", teste);
+
     fclose(file);
+}
+
+void adiciona(acertos* acerto, char teste[30]){
+    acertos* nova;
+    nova = (acertos *) malloc(sizeof (acertos));
+    for(int i = 0; 0 < 30; i++){
+        nova->notas[i] = teste[i];
+        printf("%s", nova->notas[i]);
+    }
+    nova->next = acerto;
+    acerto = nova;
 }
