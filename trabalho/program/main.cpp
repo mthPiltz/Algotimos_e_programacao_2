64d12a3707ef38c2dc58c1f5b4d1110795f0db05
Piltz <matheus.piltz@ufms.br>
2022-10-19 20:24:20 -0400

    Trabalho: modelagem das structs dos arquivos

--- trabalho/program/main.cpp
+++ trabalho/program/main.cpp
@@ -1,45 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-struct acertos {
-    acertos* next;
-    char notas[30];
+struct cursos_pesos{
+    int codigo;
+    char nome_curso[50];
+    int redacao;
+    int matematica;
+    int linguagens;
+    int humanas;
+    int natureza;
+};
+struct cursos_vagas{
+    int codigo;
+    int AC;
+    int L1;
+    int L3;
+    int L4;
+    int L5;
+    int L7;
+    int L8;
+    int L9;
+    int L11;
+    int L13;
+    int L15;
+};
+struct dados{
+    int codigo_curso;
+    int inscricao_aluno;
+    char nome_aluno[81];
+    char data_nascimento[11];
+};
+struct acertos{
+    int inscricao_aluno;
+    int redacao;
+    int matematica;
+    int linguagens;
+    int humanas;
+    int natureza;
+};
+struct altera_nota_redacao{
+    int inscricao_candidato;
+    int nota_anterior;
+    int nota_alterada;
 };
 
-void adiciona(acertos* acerto, char teste[30]);
 
 int main(){
-    FILE * file;
-    file = fopen("acertos.txt", "r");
-    char teste[30];
-
+    printf("a");
+    // FILE * file = fopen("acertos copy.txt", "r");
+    FILE * file = fopen("D:/Estudo/UFMS/4°SEMESTRE/PROG2/trabalho/program/arquivos_entradas/acertos.txt", "r");
     if(file == NULL){
-        system("pause");
-        return 0;
-    }
-
-    int i = 0;
-    acertos* acerto;
-
-    while (fgets(teste, 1500, file) != NULL)
-    {
-        adiciona(acerto, teste);
-        break;
     }
     
-    printf("[%s]", teste);
-
     fclose(file);
-}
-
-void adiciona(acertos* acerto, char teste[30]){
-    acertos* nova;
-    nova = (acertos *) malloc(sizeof (acertos));
-    for(int i = 0; 0 < 30; i++){
-        nova->notas[i] = teste[i];
-        printf("%s", nova->notas[i]);
-    }
-    nova->next = acerto;
-    acerto = nova;
 }
